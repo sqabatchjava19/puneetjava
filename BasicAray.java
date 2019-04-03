9014a76bc8b6bf07b95da71270344d5ddbcf3a38
puneet <puneet.phpdeveloper@gmail.com>
2019-04-03 12:28:32 +0530

    add new file in git

---
+++ BasicAray.java
@@ -0,0 +1,38 @@
+package ArrayBasic;
+
+import java.util.*;
+public class BasicAray {
+
+	public static void main(String[] args) {
+		
+		int arr[] = {10,20,30,40,50,60,70,80};
+		System.out.println(Arrays.toString(arr)); // [10, 20, 30, 40, 50, 60, 70, 80]
+		
+		
+		int arr2[][] = new int[5][5];
+		System.out.println(Arrays.toString(arr2[2])); // [0, 0, 0, 0, 0]
+		
+		int arr3[][] = new int[5][];
+		System.out.println(Arrays.toString(arr3[2])); // null
+		System.out.println(Arrays.toString(arr3)); // [null, null, null, null, null]
+		
+		int arr4[][] = new int[5][5];
+		System.out.println(Arrays.toString(arr4)); // [[I@15db9742, [I@6d06d69c, [I@7852e922, [I@4e25154f, [I@70dea4e]
+	
+		// it returns memory address of array elements.
+		
+		
+		// Read single Array
+		
+		for(int val:arr)
+		{
+			System.out.println(val);
+		}
+		
+		// read 4th index value of arr array
+		
+		System.out.println(arr[3]);
+	
+	}
+
+}
